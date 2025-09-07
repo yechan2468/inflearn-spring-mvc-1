221cbc03b9ccc2988c4abe666678ff69c8c7c12b
yechan <yechan24680@gmail.com>
2025-09-07 13:56:16 +0900

    section3 15

---
+++ src/main/java/yechan/inflearn_spring_mvc_1/basic/request/RequestParamServlet.java
@@ -0,0 +1,23 @@
+package yechan.inflearn_spring_mvc_1.basic.request;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) {
+        request.getParameterNames()
+                .asIterator()
+                .forEachRemaining(name -> System.out.println(name + " " + request.getParameter(name)));
+    }
+    /*
+    GET /request-param?username=yechan&age=20
+
+    username yechan
+    age 20
+     */
+}
