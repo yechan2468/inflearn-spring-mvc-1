27433b2c39c422681228a7859d247f646cec64b0
yechan <yechan24680@gmail.com>
2025-09-08 15:13:52 +0900

    section4 23

---
+++ src/test/java/yechan/inflearn_spring_mvc_1/domain/MemberRepositoryTest.java
@@ -0,0 +1,40 @@
+package yechan.inflearn_spring_mvc_1.domain;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberRepositoryTest {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void save() {
+        Member member = new Member("hello", 20);
+
+        Member saved = memberRepository.save(member);
+
+        assertThat(memberRepository.findById(saved.getId())).isEqualTo(saved);
+    }
+
+    @Test
+    void findAll() {
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 20);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findAll();
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+}
